2319554ce182ad784e7980248cff28f5db9cc618
Katy Daviscourt and Nicole Sanders <katy+nicole@>
2017-02-21 16:15:49 -0800

    make views files

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace AnimalShelter
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => {
+                List<Type> AllTypes = Type.GetAll();
+                return View["index.cshtml", AllTypes];
+            };
+
+            Get["/animals"] = _ => {
+                List<Animal> AllAnimals = Animal.GetAll();
+                return View["animals.cshtml", AllAnimals];
+            };
+
+            Get["/types"] = _ => {
+                List<Type> AllTypes = Type.GetAll();
+                return View["types.cshtml", AllTypes];
+            };
+
+            Get["/types/new"] = _ => {
+                return View["types_form.cshtml"];
+            };
+
+            Post["/types/{id}/animals/sort"] = parameters => {
+                Dictionary<string, object> model = new Dictionary<string, object>();
+                var SelectedType = Type.Find(parameters.id);
+                List<Animal> AllSortedTypeAnimals = SelectedType.GetSortedTypeAnimals();
+                model.Add("type", SelectedType);
+                model.Add("animals", AllSortedTypeAnimals);
+                return View["type.cshtml", model];
+            };
+
+            Post["/types/new"] = _ => {
+                Type newType = new Type(Request.Form["type-name"]);
+                newType.Save();
+                return View["success.cshtml"];
+            };
+
+            Get["/animals/new"] = _ => {
+                List<Type> AllTypes = Type.GetAll();
+                return View["animals_form.cshtml", AllTypes];
+            };
+            Post["/animals/new"] = _ => {
+                Animal newAnimal = new Animal(Request.Form["animal-name"], Request.Form["animal-gender"], Request.Form["animal-date"], Request.Form["animal-breed"], Request.Form["animal-type-id"]);
+                newAnimal.Save();
+                return View["success.cshtml"];
+            };
+            Post["/animals/delete"] = _ => {
+                Animal.DeleteAll();
+                return View["cleared.cshtml"];
+            };
+            Get["/types/{id}"] = parameters => {
+                Dictionary<string, object> model = new Dictionary<string, object>();
+                var SelectedType = Type.Find(parameters.id);
+                var TypeAnimals = SelectedType.GetAnimals();
+                model.Add("type", SelectedType);
+                model.Add("animals", TypeAnimals);
+                return View["type.cshtml", model];
+            };
+        }
+    }
+}
